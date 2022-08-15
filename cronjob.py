4e592120e9e4a6bb255b1a2e743363aa9cf43443
Patricio Ernesto Antonio Arena <patricio.e.arena@gmail.com>
2022-08-15 20:34:00 -0300

    other refactor xD

--- cronjob.py
+++ cronjob.py
@@ -3,16 +3,16 @@ import os
 from apscheduler.schedulers.blocking import BlockingScheduler
 from main import main
 
-type_job = os.environ['TYPE_JOB']
-
-scheduler = BlockingScheduler(timezone=os.environ['TIMEZONE'])
-
-if type_job == 'interval':
-    scheduler.add_job(main, "interval", minutes=int(os.environ['SCHULER_MINUTES']))
+try:
+    type_job = os.environ['TYPE_JOB']
+    scheduler = BlockingScheduler(timezone=os.environ['TIMEZONE'])
+    if type_job == 'interval':
+        scheduler.add_job(main, "interval", minutes=int(os.environ['SCHULER_MINUTES']))
+    elif type_job == 'cron':
+        hour_minutes = os.environ['RUN_TIME'].split(':')
+        scheduler.add_job(main, 'cron', day_of_week='mon-fri', hour=int(hour_minutes[0]), minute=int(hour_minutes[1]))
     scheduler.start()
-elif type_job == 'cron':
-    print('scheduled_job')
-    scheduler.scheduled_job('cron', day_of_week='mon-fri', hour=os.environ['RUN_TIME'])
-    scheduler.start()
-else:
-    print('Job type not set for scheduler')
+
+except Exception as e:
+    print(e)
+
